b2952613c918088e5e10fe87a8989ea10ac30ef2
raju <rajukethavath4778@gmail.com>
2024-05-16 19:08:41 +0530

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import logo from '../MovieLogo copy.png'
 function NavBar() {
   return (
     <div className='flex border space-x-8 items-center pl-3 py-4' >
-    
+    <img src= {logo} className='w-[50px]'/>
    <h3 className='text-blue-400'>Movie</h3>
    <h3 className='text-blue-400'>Watchlist</h3>
   </div>
