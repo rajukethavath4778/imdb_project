af524dc2e3a2b9f8ac5fd9abc91e692fb498a4e0
raju <rajukethavath4778@gmail.com>
2024-05-16 12:18:12 +0530

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ function NavBar() {
   return (
     <div className='flex border space-x-8 items-center pl-3 py-4' >
     
-   <h3 className='text-blue-400'>Movies</h3>
+   <h3 className='text-blue-400'>Movie</h3>
    <h3 className='text-blue-400'>Watchlist</h3>
   </div>
   )
