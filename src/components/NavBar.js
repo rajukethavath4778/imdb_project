ef773d2cbccbc8f53c3223ff385cc4acfd443db0
raju <rajukethavath4778@gmail.com>
2024-05-16 12:10:47 +0530

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='flex border space-x-8 items-center pl-3 py-4' >
+    
+   <h3 className='text-blue-400'>Movies</h3>
+   <h3 className='text-blue-400'>Watchlist</h3>
+  </div>
+  )
+
+}
+
+export default NavBar
